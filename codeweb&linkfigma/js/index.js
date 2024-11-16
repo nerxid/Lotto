ac6aba7c07820d4ed04b74e47d039da3bc8368a5
nerxid <134153856+nerxid@users.noreply.github.com>
2024-11-16 15:50:47 +0700

    one-commit

---
+++ codeweb&linkfigma/js/index.js
@@ -0,0 +1,22 @@
+function showPopup() {
+    document.querySelector('.lay-out-popup').style.display = 'block';
+}
+
+function hidePopup() {
+    document.querySelector('.lay-out-popup').style.display = 'none';
+}
+
+function showPopup2() {
+    document.querySelector('.lay-out-popup2').style.display = 'block';
+}
+
+function hidePopup2() {
+    document.querySelector('.lay-out-popup2').style.display = 'none';
+}
+
+
+function genqr() {
+    document.querySelector('.QR-CODE').style.display = 'block';
+    document.querySelector('.text-scan').style.display = 'flex';
+    
+}
